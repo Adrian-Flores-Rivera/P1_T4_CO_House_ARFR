52064c3e56501f22ecb877a3cc582556431f9d02
Adrian-Flores-Rivera <A269990@alumnos.uaslp.mx>
2023-02-02 23:10:39 -0600

    Parcial 1 Tarea 4 Clases y objetos ARFR

---
+++ src/RestRoom.java
@@ -0,0 +1,22 @@
+public class RestRoom {
+    private boolean showerCabin;
+    private int width;
+    private int height;
+    private String color;
+
+    public boolean isShowerCabin() {
+        return showerCabin;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
